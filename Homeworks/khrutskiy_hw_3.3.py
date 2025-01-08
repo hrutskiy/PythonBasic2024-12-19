95169fbe4187305e284176f65b0fe62814d2f1ea
khrutskiy <xpytskiy@ukr.net>
2025-01-08 17:38:04 +0200

    Python Basic khrutskiy_hw_3.2.py

---
+++ Homeworks/khrutskiy_hw_3.3.py
@@ -0,0 +1,24 @@
+lst = [1, 2, 3, 4, 5, 6]
+a = (len(lst) + 1) // 2
+separated_lst = [lst[:a], lst[a:]]
+print(separated_lst)
+
+lst = [1, 2, 3]
+a = (len(lst) + 1) // 2
+separated_lst = [lst[:a], lst[a:]]
+print(separated_lst)
+
+lst = [1, 2, 3, 4, 5]
+a = (len(lst) + 1) // 2
+separated_lst = [lst[:a], lst[a:]]
+print(separated_lst)
+
+lst = [1]
+a = (len(lst) + 1) // 2
+separated_lst = [lst[:a], lst[a:]]
+print(separated_lst)
+
+lst = []
+a = (len(lst) + 1) // 2
+separated_lst = [lst[:a], lst[a:]]
+print(separated_lst)
