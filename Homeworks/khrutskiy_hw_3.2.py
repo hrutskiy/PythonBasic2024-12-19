2d77ad862249489cfd5326d849265d73fd8efba4
khrutskiy <xpytskiy@ukr.net>
2025-01-08 17:37:47 +0200

    Python Basic khrutskiy_hw_3.2.py

---
+++ Homeworks/khrutskiy_hw_3.2.py
@@ -0,0 +1,24 @@
+lst=[12, 3, 4, 10]
+if len(lst)>1:
+    lst = [lst[-1]] + lst[:-1]
+    print(lst)
+else:
+    print(lst)
+lst=[1]
+if len(lst)>1:
+    lst = [lst[-1]] + lst[:-1]
+    print(lst)
+else:
+    print(lst)
+lst=[]
+if len(lst)>1:
+    lst = [lst[-1]] + lst[:-1]
+    print(lst)
+else:
+    print(lst)
+lst=[12, 3, 4, 10, 8]
+if len(lst)>1:
+    lst = [lst[-1]] + lst[:-1]
+    print(lst)
+else:
+    print(lst)
