f34275552869b069cdf9e588350cf3e611c0e9c0
khrutskiy <xpytskiy@ukr.net>
2025-01-12 18:32:48 +0200

    Python Basic khrutskiy_hw_4.3.py

--- Homeworks/khrutskiy_hw_2.2.py
+++ Homeworks/khrutskiy_hw_2.2.py
@@ -1,8 +1,8 @@
 number = int(input("Введіть п'ятизначне число: "))
-digit1 = number % 10              # Остання цифра
-digit2 = (number // 10) % 10      # Четверта цифра
-digit3 = (number // 100) % 10     # Третя цифра
-digit4 = (number // 1000) % 10    # Друга цифра
-digit5 = (number // 10000) % 10   # Перша цифра
+digit1 = number % 10
+digit2 = (number // 10) % 10
+digit3 = (number // 100) % 10
+digit4 = (number // 1000) % 10
+digit5 = (number // 10000) % 10
 reversed_number = digit1 * 10000 + digit2 * 1000 + digit3 * 100 + digit4 * 10 + digit5
 print("Перевернуте число:", reversed_number)
