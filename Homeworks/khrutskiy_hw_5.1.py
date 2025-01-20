a5d6493f1047ed96b1e2c1bdfcec4bbc059f40de
khrutskiy <xpytskiy@ukr.net>
2025-01-20 19:04:47 +0200

    Python Basic khrutskiy_hw_5.1.py

--- Homeworks/khrutskiy_hw_5.1.py
+++ Homeworks/khrutskiy_hw_5.1.py
@@ -1,17 +1,21 @@
-import string
 import keyword
+import string
 user_input = input("Введіть ім'я змінної: ")
 is_valid = True
 if user_input in keyword.kwlist:
     is_valid = False
-if user_input.count("__") > 0:
+elif not user_input:
     is_valid = False
-allowed_chars = string.ascii_lowercase + string.digits + "_"
-for char in user_input:
-    if char not in allowed_chars:
-        is_valid = False
-if user_input and user_input[0].isdigit():
+elif user_input == "__":
     is_valid = False
-if not user_input:
+elif "__" in user_input:
     is_valid = False
+elif user_input[0].isdigit():
+    is_valid = False
+else:
+    allowed_chars = string.ascii_lowercase + string.digits + "_"
+    for char in user_input:
+        if char not in allowed_chars:
+            is_valid = False
+            break
 print(is_valid)
