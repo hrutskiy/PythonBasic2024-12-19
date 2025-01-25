31ce49ac11baf880b01aa1b91b5245b248933031
khrutskiy <xpytskiy@ukr.net>
2025-01-25 16:20:23 +0200

    Python Basic khrutskiy_hw_5.3.py

--- Homeworks/khrutskiy_hw_5.3.py
+++ Homeworks/khrutskiy_hw_5.3.py
@@ -4,7 +4,7 @@ user_input = input("Введіть рядок: ")
 cleaned_input = ''.join(char if char not in string.punctuation else ' ' for char in user_input)
 words = cleaned_input.split()
 hashtag = ''.join(word.capitalize() for word in words)
-if len(hashtag) > 140:
-    hashtag = hashtag[:140]
+hashtag = hashtag[:139]
 hashtag = '#' + hashtag
 print(hashtag)
+print(len(hashtag))
